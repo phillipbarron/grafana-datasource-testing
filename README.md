6b5bec0aef044d2a4c672ffbdc107d1f83b4d71b
Phill Barron <phillip.barron@guardian.co.uk>
2022-12-22 09:20:15 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+```bash
+docker run -d -p 3000:3000 --name grafana grafana/grafana:7.5.0
+```
+
+# ssh in to instance
+
+docker exec -it grafana /bin/bash
+
+# Install Json plugin for grafana
+
+```bash
+grafana-cli plugins install simpod-json-datasource
+```
+# Restart Container
+
+```bash
+exit
+docker restart grafana
+```
+
+# Setup local data API using Dev nginx
+
+[update nginx config](nginx-conf.yaml) and setup locally
+
+```bash
+dev-nginx setup-app nginx-conf.yaml
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+const allowList = ["https://grafana.local.dev-gutools.co.uk"];
+
+const corsOptions = {
+  origin: allowList,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const getFakeData = (metric, numberOfDataPoints, range) => {
+  const data = [];
+  const timeNow = new Date().getTime();
+  const oneHourInMilliseconds = 1000 * 60 * 60;
+  for (let i = 0; i < numberOfDataPoints; i++) {
+    data.push([Math.floor(Math.random() * range), timeNow - i * oneHourInMilliseconds]);
+  }
+  return { target: metric, datapoints: data };
+};
+
+app.get("/", (_, res) => {
+  res.send("ok");
+});
+
+app.post("/search", (_, res) => {
+  res.json(["uniqueUsers", "claimedMessges", "unclaimedMessages"]);
+});
+
+app.post("/query", (req, res) => {
+  const { targets } = req.body;
+  const response = [];
+  targets?.forEach((target) => {
+    const { target: metric } = target;
+    response.push(getFakeData(metric, 30, 1000));
+  });
+  console.log('resp', JSON.stringify(response, null, 2));
+  res.json(response);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
